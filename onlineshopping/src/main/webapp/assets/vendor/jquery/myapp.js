4614b2d6a4722b9ae3d851b94462405ee6dbd44d
Rajesh Ittikala <rajeshittikala369@gmail.com>
2017-11-04 13:45:32 +0530

    Completed the interface theme and other issues

---
+++ onlineshopping/src/main/webapp/assets/vendor/jquery/myapp.js
@@ -0,0 +1,18 @@
+$(function() {
+	// Active Solving Menu Problem
+	
+	switch(menu) {
+	  case 'About Us' : 
+	         $('#about').addClass('active');
+	         break;
+	  case 'Contact Us' : 
+	         $('#contact').addClass('active');
+	         break;
+	  default: 
+	         $('#home').addClass('active');
+	         break;       
+	}
+});
+
+
+
